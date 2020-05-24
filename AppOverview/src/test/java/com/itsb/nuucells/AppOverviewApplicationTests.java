8785b6460a9729f9070ad43e7c465f968a2aef70
m48251 <m48251@W7N22046.tdk.dk>
2020-05-24 13:56:48 +0200

    My First Commit

---
+++ AppOverview/src/test/java/com/itsb/nuucells/AppOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsb.nuucells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
